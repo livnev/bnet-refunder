274224ef3f90b1231ffab2478bb568939aa0aace
Lev Livnev <lev@dapp.org>
2025-06-05 14:40:18 +0100

    BnetRefunder.sol: rename

--- src/BnetRefunder.sol
+++ src/BnetRefunder.sol
@@ -29,9 +29,9 @@ contract BnetRefunder is MerkleProof {
     address public owner;
     mapping(uint256 => bytes32) public roots;
     mapping(uint256 => mapping(uint256 => uint256)) public claimedBitMap;
-    uint256 epoch;
+    uint256 latest;
 
-    event Claim(uint256 epoch_, uint256 index, uint256 amount) anonymous;
+    event Claim(uint256 epoch, uint256 index, uint256 amount) anonymous;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "not owner");
@@ -42,17 +42,17 @@ contract BnetRefunder is MerkleProof {
         owner = msg.sender;
     }
 
-    function publish(uint256 epoch_, bytes32 root) external payable onlyOwner {
+    function publish(uint256 epoch, bytes32 root) external payable onlyOwner {
         // desire invariant msg.value = sum(leaf), but this is not checked here
         // if violated, race condition can occur
-        // roots can be updated, but only for current epoch
-        require(epoch_ >= epoch, "invalid epoch");
-        if (epoch_ != epoch) epoch = epoch_;
-        roots[epoch_] = root;
+        // roots can be updated, but only for latest epoch
+        require(epoch >= latest, "invalid epoch");
+        if (epoch != latest) latest = epoch;
+        roots[epoch] = root;
     }
 
     function claim(
-        uint256 epoch_,
+        uint256 epoch,
         uint256 index,
         address payable account,
         uint256 amount,
@@ -61,20 +61,20 @@ contract BnetRefunder is MerkleProof {
         // check leaf wasn't claimed
         uint256 claimedWordIndex = index / 256;
         uint256 claimedBitIndex = index % 256;
-        uint256 claimedWord = claimedBitMap[epoch_][claimedWordIndex];
+        uint256 claimedWord = claimedBitMap[epoch][claimedWordIndex];
         uint256 mask = (1 << claimedBitIndex);
         require(claimedWord & mask == 0, "already claimed");
 
         // mark leaf as claimed
-        claimedBitMap[epoch_][claimedWordIndex] = claimedWord | mask;
+        claimedBitMap[epoch][claimedWordIndex] = claimedWord | mask;
 
         // verify proof
         bytes32 leaf = keccak256(abi.encodePacked(index, account, amount));
-        require(verifyProof(merkleProof, roots[epoch_], leaf), "invalid proof");
+        require(verifyProof(merkleProof, roots[epoch], leaf), "invalid proof");
 
         // pay claim
         account.transfer(amount);
 
-        emit Claim(epoch_, index, amount);
+        emit Claim(epoch, index, amount);
     }
 }
